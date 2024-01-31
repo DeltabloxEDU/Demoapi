ab81c99e65deacd4264dbccde0a33c09ba8360b0
Divyesh Patoriya <divyesh@deltablox.com>
2024-01-31 19:59:39 +0530

    fixed with logging

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,106 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use Log;
 class UserController extends Controller
 {
     
-    public function index()
+    public function index(Request $request)
     {
+        Log::info(json_encode($request->all()));
+        return $users = User::latest()->first();
+    }
+ 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+ 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+ 
+        $user = User::create($request->all());
+        return $user;
+    }
+ 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user,$id)
+    {
+        echo $id;exit;
+        return $users = User::where("id",$id)->first();
+       
+    }
+ 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+ 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        $user->update($request->all());
+
         return [
-            "user_name" => "Deltablox",
-            "user_code" => "123"
+            "status" => 1,
+            "data" => $user,
+            "msg" => "User updated successfully"
+        ];
+ 
+ 
+    }
+ 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return [
+            "status" => 1,
+            "data" => $user,
+            "msg" => "User deleted successfully"
         ];
     }
 }
